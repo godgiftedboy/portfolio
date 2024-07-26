cefd4283984bf7638e4bdaa0f5feed7c5a71f09e
rajprajapati12321 <rajprajapati@redwire.com.np>
2024-07-26 15:12:44 +0545

    minor changes for positioning

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -143,27 +143,25 @@ export const BentoGridItem = ({
               </div>
             </div>
           )}
+
+          
         {id === 6 && (
-            <div className="mt-5 relative">
-              {/* button border magic from tailwind css buttons  */}
-              {/* add rounded-md h-8 md:h-8, remove rounded-full */}
-              {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
-              {/* add handleCopy() for the copy the text */}
+            <div className="mt-5 relative">          
               <div
-                className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
+                className={`absolute -bottom-8 right-0 ${copied ? "block" : "block"
                   }`}
               >
                 {/* <img src="/confetti.gif" alt="confetti" /> */}
                 <Lottie options={defaultOptions} height={200} width={400} />
-              </div>
-
-              <MagicButton
+                <MagicButton
                 title={copied ? "Email is Copied!" : "Copy my email address"}
                 icon={<IoCopyOutline />}
                 position="left"
                 handleClick={handleCopy}
-                otherClasses="!bg-[#161A31]"
+                otherClasses="!bg-[#161A31]"  
               />
+              </div>
+              
             </div>
           )}
 
